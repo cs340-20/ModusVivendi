70e6134aea909d950fa6e9cab2ae674d8c83acad
Dustin Craig <dustin@dustinccraig.com>
2020-01-24 09:17:18 -0500

    Added navbar component. No real styling just testing git

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Navbar from './components/navbar/navbar'
 import Customers from './components/customers/customers'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Navbar />
         <Customers />
       </header>
     </div>
---
+++ client/src/components/navbar/navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import "./navbar.css"
+
+export default class Navbar extends Component {
+
+    render() {
+        return (
+            <h3>Test</h3>
+        )
+    }
+}
