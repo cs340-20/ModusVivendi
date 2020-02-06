4a7adf384ec0834e81f787b8069bea5b4ee565a1
Dustin Craig <dustin@dustinccraig.com>
2020-02-06 11:33:12 -0500

    Started on navbar

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
 import Navbar from 'react-bootstrap/Navbar'
-import logo from '../../images/MV_ondark.png'
+import logo from '../../images/MV_resize.png'
 import "./navbar.css"
-
-let navbar_style = {
-    width: '80px',
-    height: '80px'
-}
+import { Nav } from 'react-bootstrap'
 
 export default class MainNavbar extends Component {
 
     render() {
         return (
             <Navbar bg="dark" expand="lg">
-                <Navbar.Brand href="#"><img src={logo} style={navbar_style}></img>ModusVivendi</Navbar.Brand>
+                <Navbar.Brand href="#"><img src={logo} alt="ModusVivendi"></img></Navbar.Brand>
+                <Navbar.Collapse id="nav_collapse" className="pull-right">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#">Home</Nav.Link>
+                        <Nav.Link href="#">Diet</Nav.Link>
+                        <Nav.Link href="#">Workout</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         )
     }
