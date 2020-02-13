2be2af0f9fbe60b9a7238f44ceef74c928a16047
Jimmy Neville <jneville0815@yahoo.com>
2020-02-13 11:13:43 -0500

    fixed conflict

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import MainNavbar from './components/navbar/navbar'
 import Customers from './components/customers/customers'
 import Registration from './components/Registration/Registration'
-<<<<<<< HEAD
-import Loginpage from './components/loginpage/login'
-=======
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
->>>>>>> ba98998815cc3f2d516548e074673d7d9a39279e
 
 // <MainNavbar />     
 //       <Registration />
@@ -17,12 +13,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <MainNavbar />     
-      <Registration />
-      <Customers />
-      <Loginpage/>
-=======
     <MainNavbar />
     <Router>
         <Switch>
@@ -31,7 +21,6 @@ function App() {
             </Route>
         </Switch>
     </Router>
->>>>>>> ba98998815cc3f2d516548e074673d7d9a39279e
     </div>
   );
 }
