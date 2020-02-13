21d090b9d85fb3d792d2a2ab97800e0136bf8324
Dustin Craig <dustin@dustinccraig.com>
2020-02-13 12:10:58 -0500

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,22 @@
 import React from 'react'
 import './App.css'
-// import './bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import MainNavbar from './components/navbar/navbar'
 import Customers from './components/customers/customers'
 import Registration from './components/registration/Registration'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-// <MainNavbar />     
-//       <Registration />
-
 function App() {
-  return (
+    return (
     <div className="App">
-    <MainNavbar />
-    <Router>
-        <Switch>
-            <Route path="/register">
-                <Registration />
-            </Route>
-        </Switch>
-    </Router>
+        <MainNavbar />
+        <Router>
+            <Switch>
+                <Route path="/register">
+                    <Registration />
+                </Route>
+            </Switch>
+        </Router>
     </div>
   );
 }
