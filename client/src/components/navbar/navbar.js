e69832af46b6a82e73bce6f042b1a6c0bd38ef51
Dustin Craig <dustin@dustinccraig.com>
2020-02-17 09:44:47 -0500

    fixed navbar error

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -12,24 +12,24 @@ export default class MainNavbar extends Component {
     render() {
         return (  
         <div>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-1">
-                <a class="navbar-brand" href="#"><img src={logo} alt="ModusVivendi"></img></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-1">
+                <a className="navbar-brand" href="#"><img src={logo} alt="ModusVivendi"></img></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
                         <Link to="/" className="nav-link">Home</Link>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <Link to="/diet" className="nav-link">Diet</Link>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <Link to="/workout" className="nav-link" type="submit" to="/workout">Workout</Link>
                     </li>
                     </ul>
-                    <form class="form-inline my-2 my-lg-0">
+                    <form className="form-inline my-2 my-lg-0">
                     <Link className="btn btn-outline-success my-2 my-sm-0" type="submit" to="/login">
                        Login/Register
                     </Link>
