419ba9180cd3ac7baf34ca26864bcaf2f4a7c23e
clampe1 <46681742+clampe1@users.noreply.github.com>
2020-02-28 09:13:27 -0500

    Update home.js

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
-import './Home.css'
+import './home.css'
 
 export default class Home extends Component {
 
